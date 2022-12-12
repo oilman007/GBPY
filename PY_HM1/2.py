09c8286a194ede99be792b003cc9e013c2029373
Artur <aubakirov-artur@yandex.ru>
2022-12-12 11:05:55 +0700

    fix 2.py

--- PY_HM1/2.py
+++ PY_HM1/2.py
@@ -2,11 +2,13 @@
 # (расшифровка этого выражения not (x[0] or x[1] or x[2] = not x[0] and not x[1] and not x[2]) для всех значений предикат.
 
 
-x1 = int(input('enter x1 '))
-x2 = int(input('enter x2 '))
-x3 = int(input('enter x3 '))
+x1 = bool(input('enter x1 '))
+x2 = bool(input('enter x2 '))
+x3 = bool(input('enter x3 '))
 
-r1 = not (x1 or x2 or x3)
-r2 = not x1 and not x2 and not x3
+c1 = not (x1 or x2 or x3)
+c2 = not x1 and not x2 and not x3
 
-print(f'result is {r1==r2}')
+print(f'condition#1 is {c1} = not ({x1} or {x2} or {x3})')
+print(f'condition#2 is {c2} = not {x1} and not {x2} and not {x3}')
+print(f'condition#1 == condition#2 is {c1==c2}')
